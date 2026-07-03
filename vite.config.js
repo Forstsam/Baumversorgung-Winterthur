752a949485ee4903a0e10bb445f614da16e217db
Forstsam <samuel.schweizer@swissonline.ch>
2026-07-03 12:24:13 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: './' erzeugt relative Pfade -> funktioniert sowohl auf Netlify/Vercel (Wurzel)
-// als auch auf GitHub Pages (Unterpfad /Baumversorgung-Winterthur/), ohne Änderung.
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
